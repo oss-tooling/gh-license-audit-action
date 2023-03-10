4bcceb00dc1423111c0e9dc0c2f05bf502b06bb7
Tyler Garner <garnertb@gmail.com>
2023-03-10 11:51:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Enterprise License Audit Action
+
+Action that returns license counts for GitHub Enterprises.
+
+## Usage
+
+```
+uses: oss-tooling/license-audit-action@v1
+with:
+  enterprise: <name-of-your-enterprise>
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+
+const { Octokit } = require("@octokit/rest");
+const core = require('@actions/core');
+const { retry } = require('@octokit/plugin-retry')
+const { throttling } = require('@octokit/plugin-throttling')
+
+const _Octokit = Octokit.plugin(retry, throttling)
+
+const login = async () => {
+  const client = new _Octokit({
+    auth: core.getInput('token', {required: true}),
+    throttle: {
+      onRateLimit: (retryAfter, options) => {
+        octokit.log.warn(
+          `Request quota exhausted for request ${options.method} ${options.url}`
+        );
+  
+        // Retry twice after hitting a rate limit error, then give up
+        if (options.request.retryCount <= 2) {
+          console.log(`Retrying after ${retryAfter} seconds!`);
+          return true;
+        }
+      },
+      onSecondaryRateLimit: (retryAfter, options, octokit) => {
+        // does not retry, only logs a warning
+        octokit.log.warn(
+          `Secondary quota detected for request ${options.method} ${options.url}`
+        );
+      }},
+  });
+
+  const licenses = await client.paginate('GET /enterprises/{enterprise}/consumed-licenses', {
+    enterprise: core.getInput('enterprise'),
+    per_page: 100
+  })
+  let dotcomUsers = 0
+  let serverUsers = 0
+  let visualStudioUsers = 0
+  let duplicates = 0
+  let accounts = 0
+
+  for (const batch of licenses) {
+      for (const user of batch.users) {
+          if (user.github_com_user) {
+              dotcomUsers++
+          }
+          if (user.enterprise_server_user) {
+              serverUsers++
+          }
+          if (user.visual_studio_subscription_user) {
+              visualStudioUsers++
+          }
+          if (user.github_com_user && user.enterprise_server_user) {
+              duplicates++
+          }
+          accounts += user.total_user_accounts
+      }
+  }
+
+  core.setOutput('total-seats-purchased', licenses[0].total_seats_purchased)
+  core.setOutput('total-seats-consumed', licenses[0].total_seats_consumed)
+  core.setOutput('dot-com-users', dotcomUsers)
+  core.setOutput('server-users', serverUsers)
+  core.setOutput('visual-studio-users', visualStudioUsers)
+  core.setOutput('duplicates', duplicates)
+  core.setOutput('total-users', dotcomUsers + serverUsers - duplicates)
+  core.setOutput('total-accounts', accounts)
+}
+
+login()
+
